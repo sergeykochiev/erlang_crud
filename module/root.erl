50708a132bdac4b14bf27710a2baf240d6d3d290
sergeykochiev <sergeyivanovo547@gmail.com>
2025-04-04 03:31:38 +0300

    added default route which responds with schema.json

--- module/root.erl
+++ module/root.erl
@@ -5,7 +5,12 @@
 -include_lib("inets/include/httpd.hrl").
 
 do(M) when M#mod.request_uri == "/" ->
-    {break, [{response, {404, ""}}]};
+    case file:read_file("../schema/schema.json") of
+        {error, _} ->
+            {break, [{response, {500, "500 INTERNAL SERVER ERROR"}}]};
+        {ok, Bin} ->
+            {break, [{response, {200, binary_to_list(Bin)}}]}
+    end;
 do(M) ->
     [_ | S] = string:split(M#mod.request_uri, "/", all),
     {proceed, S}.
